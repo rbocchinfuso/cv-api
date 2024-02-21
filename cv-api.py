0e5ba431e3fe8880e1a6c0c98d9a6f8d4295a42f
Richard  Bocchinfuso <rbocchinfuso@gmail.com>
2024-02-21 08:57:02 -0500

    auth workaround

--- cv-api.py
+++ cv-api.py
@@ -6,7 +6,7 @@
 # Generic/Built-in
 import json, configparser
 from flask import Flask, jsonify, make_response, g
-from flask_httpauth import HTTPTokenAuth
+# from flask_httpauth import HTTPTokenAuth
 # import apminsight
 
 # Owned
@@ -25,42 +25,42 @@ config.read('config.ini')
 config.sections()
 
 app = Flask(__name__)
-admin_auth = HTTPTokenAuth(scheme='Bearer')
-subscriber_auth = HTTPTokenAuth(scheme='Bearer')
+# admin_auth = HTTPTokenAuth(scheme='Bearer')
+# subscriber_auth = HTTPTokenAuth(scheme='Bearer')
 
 fmt = '%H:%M:%S' # ex. 20110104172008 -> Jan. 04, 2011 5:20:08pm 
 
-admin_tokens = {
-    config['auth']['admin_token'].encode('ascii') : "admin"
- }
+# admin_tokens = {
+    # config['auth']['admin_token'].encode('ascii') : "admin"
+ # }
 
-subscriber_tokens = {
-    config['auth']['subscriber_token'].encode('ascii') : "subscriber"
- }
+# subscriber_tokens = {
+    # config['auth']['subscriber_token'].encode('ascii') : "subscriber"
+ # }
  
-@admin_auth.verify_token
-def verify_token(token):
-    if token in admin_tokens:
-        g.current_user = admin_tokens[token]
-        return True
-    return False
+# @admin_auth.verify_token
+# def verify_token(token):
+    # if token in admin_tokens:
+        # g.current_user = admin_tokens[token]
+        # return True
+    # return True
     
-@subscriber_auth.verify_token
-def verify_token(token):
-    if token in subscriber_tokens:
-        g.current_user = subscriber_tokens[token]
-        return True
-    return False
+# @subscriber_auth.verify_token
+# def verify_token(token):
+    # if token in subscriber_tokens:
+        # g.current_user = subscriber_tokens[token]
+        # return True
+    # return True
 
-@admin_auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
-    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
+# @admin_auth.error_handler
+# def unauthorized():
+    # return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+    # # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
 
-@subscriber_auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
-    # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
+# @subscriber_auth.error_handler
+# def unauthorized():
+    # return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+    # # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
 
 @app.errorhandler(400)
 def not_found(error):
@@ -76,20 +76,20 @@ with open(config['local']['json_data']) as json_file:
 
 # api get
 @app.route('/app/api/v1.0/cv', methods=['GET']) 
-@subscriber_auth.login_required
+# @subscriber_auth.login_required
 def get_cv():
     return jsonify({'cv': [cv]})
 
 # api get specific section
 @app.route('/app/api/v1.0/cv/<section>', defaults={}, methods=['GET']) 
-@subscriber_auth.login_required
+# @subscriber_auth.login_required
 def get_cvsection(section):
     return jsonify({section: [cv[section]]})
 
-# a route to the loader.io token
-@app.route("/loaderio-a1bf45ed67f8d31af6d5fc2616f65379/")
-def index():
-    return "loaderio-a1bf45ed67f8d31af6d5fc2616f65379"
+# # a route to the loader.io token
+# @app.route("/loaderio-a1bf45ed67f8d31af6d5fc2616f65379/")
+# def index():
+    # return "loaderio-a1bf45ed67f8d31af6d5fc2616f65379"
 
 if __name__ == '__main__':
     app.config['JSON_SORT_KEYS'] = False
